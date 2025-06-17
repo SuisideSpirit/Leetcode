f1aad889891fafafc950446abf5dad23331994c5
SuisideSpirit <pratiksinghnegi7@gmail.com>
2025-06-17 11:57:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.30%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,12 +4,18 @@ public:
     int solve(int idx ,vector<int>& nums){
         int n = nums.size() ;
         if(idx >= n) return 0 ;
-        if(dp[idx] != -1) return dp[idx] ; 
+        int prev = nums[0] ; 
+        int prev2 = 0 ;
+        for(int i = 0 ; i < n ; i ++){
+            int take = nums[i] ; 
+            int notTake = prev ;
+            if(i > 1) take += prev2  ;
 
-        int pick = nums[idx] + solve(idx+2 ,nums ) ;
-        int notPick = solve(idx+1,nums) ; 
-
-        return dp[idx] = max(pick , notPick) ;
+            int curr = max(take ,notTake)  ;
+            prev2 = prev ; 
+            prev = curr ;
+        }
+        return prev;
     }
     int memo(int idx ,vector<int>& nums){
         int n = nums.size() ;
@@ -24,6 +30,6 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size() ; 
         dp.resize(n+5 , -1)  ;
-        return memo(n-1,nums) ;   
+        return solve(n-1,nums) ;   
     }
 };

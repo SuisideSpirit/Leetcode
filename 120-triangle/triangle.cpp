569b6da0c4b77bad34e6e22c134f114d35b2805c
SuisideSpirit <pratiksinghnegi7@gmail.com>
2026-02-16 01:49:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (96.69%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -6,7 +6,6 @@ public:
         for(int i = n-2; i >= 0 ; i--){
             for(int j = 0 ; j < tri[i].size() ; j++){
                 tri[i][j] = tri[i][j] + min(tri[i+1][j] , tri[i+1][j+1]) ; 
-                cout<<tri[i][j]<<" ";
             }
         }
         return tri[0][0]; 

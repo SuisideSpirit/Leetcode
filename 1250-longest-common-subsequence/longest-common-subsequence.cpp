7f6af982a96d2f2406fec25e2a47cf310e3e7bd0
SuisideSpirit <pratiksinghnegi7@gmail.com>
2026-07-07 01:07:57 +0530

    Time: 50 ms (7.35%) | Memory: 27.6 MB (30.67%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -5,13 +5,13 @@ public:
     int solve(int idx1 ,int idx2 ,string & s1 ,string & s2){
         if(idx1 >= n || idx2 >= m) return 0 ; 
         if(dp[idx1][idx2] != -1) return dp[idx1][idx2] ; 
+        if (s1[idx1] == s2[idx2]) return dp[idx1][idx2] = 1 + solve(idx1 + 1, idx2 + 1, s1, s2);
         int leave1 = solve(idx1 + 1, idx2 , s1 ,s2);
         int leave2 = solve(idx1 , idx2 + 1 , s1 ,s2);
-        int take = 0 ;
-        leave1 = max(leave1 , leave2) ; 
-        if(s1[idx1] == s2[idx2]) take = 1 + solve(idx1 + 1, idx2 +1 , s1 ,s2 ) ; 
 
-        return dp[idx1][idx2] = max(take, leave1) ; 
+        leave1 = max(leave1 , leave2) ; 
+
+        return dp[idx1][idx2] = leave1 ; 
     }
     int longestCommonSubsequence(string text1, string text2) {
         n = text1.size() , m = text2.size()  ;

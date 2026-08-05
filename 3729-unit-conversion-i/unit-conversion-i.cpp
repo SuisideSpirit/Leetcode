b5bc00fe781c3b246d0c5ccf83d16b62f8428154
Pratik Singh Negi <pratiksinghnegi7@gmail.com>
2026-08-05 21:49:25 +0530

    Time: 291 ms (7.11%) | Memory: 299 MB (5.02%) - LeetSync

--- 3729-unit-conversion-i/unit-conversion-i.cpp
+++ 3729-unit-conversion-i/unit-conversion-i.cpp
@@ -2,31 +2,27 @@ class Solution {
 public:
     typedef long long ll ;
     int MOD = 1e9 + 7 ; 
+    unordered_map <int,vector<pair<int,int>>> mp;
+    void Traversal(ll curr ,ll factor , vector <int> &ans ,  vector <int>& visited){
+        if(visited[curr]) return ; 
+        ans[curr] = factor ; 
+        visited[curr] = 1 ;
+        for(auto ele : mp[curr]){
+            ll next = ele.first , mult = ele.second ; 
+            if(visited[next] == 0){
+                Traversal(next ,(factor * mult)%MOD ,ans , visited ) ; 
+            } 
+        }
+    }
     vector<int> baseUnitConversions(vector<vector<int>>& conversions) {
         int n = conversions.size() + 1 ;
-        vector <int> ans(n) ; 
-        unordered_map <int,vector<pair<int,int>>> mp; 
+        vector <int> ans(n) ;  
         for(auto ele : conversions){
             int u = ele[0], v = ele[1], mult = ele[2] ;
             mp[u].push_back({v,mult}) ;  
         }
-        queue <pair<ll,ll>> q ; 
-        q.push({0,1}) ; 
-        vector <ll> visited(n , 0) ; 
-        visited[0] = 1; 
-        while(q.size() != 0 ){
-            auto ele = q.front() ; 
-            q.pop();
-            ll curr = ele.first, quan = ele.second ; 
-            ans[curr] = quan ;
-            for(auto ele : mp[curr]){
-                ll next = ele.first , mult = ele.second ; 
-                if(visited[next] == 0){
-                    q.push({next , (quan* mult)%MOD}) ; 
-                    visited[next] = 1 ;
-                } 
-            }
-        }
+        vector <int> visited(n,0) ;
+        Traversal(0 , 1,  ans,  visited) ; 
         return ans ; 
     }
 };

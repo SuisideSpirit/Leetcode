fdb3704f4977f50439daf153d5d6cc4602641fea
Pratik Singh Negi <pratiksinghnegi7@gmail.com>
2026-08-07 01:13:18 +0530

    Time: 15 ms (3.49%) | Memory: 15.4 MB (13.01%) - LeetSync

---
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int find_parent(int curr , vector <int> &parent){
+        if(parent[curr] == -1) return -1 ;
+        if(parent[curr] == curr) return curr ; 
+        return parent[curr] = find_parent(parent[curr] , parent) ; 
+    }
+    void Union(int u , int v , vector<int> &parent , vector <int> & size){
+        int pu = find_parent(u , parent) ; 
+        int pv = find_parent(v , parent) ; 
+        if(pu == pv) return ; 
+        if(size[pu] > size[pv]){
+            size[pu] += size[pv] ; 
+            parent[pv] = pu ; 
+        }
+        else{
+            size[pv] += size[pu] ; 
+            parent[pu] = pv ; 
+        }
+    }
+    bool equationsPossible(vector<string>& equations) {
+        vector <pair<int ,int>> check; 
+        vector <int> parent(26) , size(26,1) ; 
+        for(int i = 0 ; i < 26 ; i++){
+            parent[i] = -1 ; 
+        }
+        for(auto ele : equations){
+            char a = ele[0] , b =ele[3] , c = ele[1] ; 
+            int A = a - 'a' , B = b - 'a' ;
+            if(parent[A] == -1) parent[A] = A ;
+            if(parent[B] == -1) parent[B] = B ;
+            if(c == '!'){
+                check.push_back({A , B}) ; 
+                continue ;
+            }
+            Union(A , B , parent , size) ; 
+        }
+        for(auto ele : check){
+            int x= ele.first , y = ele.second ; 
+            cout<<parent[x]<<" "<<parent[y]<<endl;
+            int px = find_parent(x ,parent) ; 
+            int py = find_parent(y , parent) ; 
+            if(px == -1 || py == -1) continue ;
+            if(px == py){
+                cout<<px<<" "<<py<<endl ;
+                return false ;
+            }
+        }
+        return true ; 
+        
+    }
+};

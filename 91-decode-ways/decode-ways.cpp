61f4339b1b181e673982e9c0ce560e07bfc9b8e4
Pratik Singh Negi <pratiksinghnegi7@gmail.com>
2026-09-02 00:10:37 +0530

    Time: 3 ms (15.62%) | Memory: 15.8 MB (5.28%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector <int> dp ;
+    int solve(int idx , string s){
+        int n = s.size() ;
+        if(idx == s.size()) return 1 ;
+        if(idx >= s.size() || s[idx] == '0') return 0 ; 
+        if(dp[idx] != -1) return dp[idx] ; 
+
+        int ans = 0 ;
+        if(s[idx] == '1'  && idx != n-1) ans += solve(idx +2 , s) ; 
+        else if(s[idx] == '2' && idx != n-1 && s[idx +1] < '7') ans +=  solve(idx +2 , s) ;
+
+        ans += solve(idx+ 1 , s) ; 
+
+        return dp[idx] = ans ;
+    }
+    int numDecodings(string s) {
+        int n = s.size() ; 
+        //if(s[0] == '0') return 0 ; 
+        dp.resize( n + 5 , -1) ; 
+        return solve(0 , s) ; 
+    }
+};

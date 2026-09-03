d3e5b94f7e79598db5f92b29d9a6e8818428ebbd
Pratik Singh Negi <pratiksinghnegi7@gmail.com>
2026-09-03 21:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 165.9 MB (31.94%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -3,24 +3,31 @@ public:
     bool uniformArray(vector<int>& nums) {
         int n = nums.size() ; 
         bool ans = true ;
-        sort(nums.begin() , nums.end()) ; 
-        bool Odd = false , Even= false ;
-        for(int i =0 ; i < n ; i++){
-            if(nums[i]%2 !=0 ){
-                ans = false ;
-                break ;
-            }
+        int mini = INT_MAX ;
+        bool Odd = false ;
+        for(int i = 0 ; i  <n ; i++){
+            mini = min(mini, nums[i]) ;
+            if(nums[i] %2 != 0) Odd = true ; 
         }
-        if(ans) return ans ;
-        ans = true ;
-        //check for odd
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i]%2 == 0 && Odd == false ){
-                ans = false; 
-                break ; 
-            }
-            else Odd = true ; 
-        }
-        return ans  ; 
+        if(mini%2 != 0) return true ;
+        if(!Odd) return true; 
+        return false; 
+        // bool Odd = false , Even= false ;
+        // for(int i =0 ; i < n ; i++){
+        //     if(nums[i]%2 !=0 ){
+        //         ans = false ;
+        //         break ;
+        //     }
+        // }
+        // if(ans) return ans ;
+        // ans = true ;
+        // //check for odd
+        // for(int i = 0 ; i < n ; i++){
+        //     if(nums[i]%2 == 0 && Odd == false ){
+        //         ans = false; 
+        //         break ; 
+        //     }
+        //     else Odd = true ; 
+        // }
     }
 };
